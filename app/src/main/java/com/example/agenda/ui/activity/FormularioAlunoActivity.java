649d3a0efe18ac73034c49bd39265b7e04f558e0
Bruno Rezende <brunoc.rez@gmail.com>
2019-04-23 21:17:34 -0300

    implementando fluxo de telas

--- app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,9 +1,16 @@
 package com.example.agenda.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.agenda.R;
+import com.example.agenda.dao.AlunoDAO;
+import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -11,5 +18,30 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        setTitle("Cadastrar Novo Aluno");
+
+        final AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno novoAluno = new Aluno(nome, telefone, email);
+                dao.salva(novoAluno);
+
+                startActivity(new Intent(FormularioAlunoActivity.this, ListaAlunosActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,11 +1,16 @@
 package com.example.agenda.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.agenda.R;
+import com.example.agenda.dao.AlunoDAO;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,12 +23,21 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        List<String> alunos = new ArrayList<>((Arrays.asList("Bruno", "Paula", "Rones", "Indyara")));
+        AlunoDAO dao = new AlunoDAO();
+
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class));
+            }
+        });
+
         ListView listaAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                alunos));
+                dao.getListaAlunos()));
 
     }
 }
